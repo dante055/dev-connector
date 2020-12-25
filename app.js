860b6cb7bd72875ab3ce49e8f67757f49c6b4023
dante055 <sonofsparda055@gmail.com>
2020-12-25 14:16:45 +0530

    Prepared for deployment

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
@@ -31,6 +32,14 @@ app.use('/api/v1', require('./routes/api/authRoutes'));
 app.use('/api/v1/users', require('./routes/api/usersRoutes'));
 app.use('/api/v1/profiles', require('./routes/api/profileRoutes'));
 app.use('/api/v1/posts', require('./routes/api/postsRoutes'));
+
+// Serve static assets in the production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.js'));
+  });
+}
 
 // @desc Unimplemented route handler
 app.all('*', (req, res, next) => {
