f792dbbdf3aa7acc21356cb9ca0478a8f194ae24
dante055 <sonofsparda055@gmail.com>
2020-12-25 14:37:46 +0530

    modified axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:8000/api/v1';
+// const baseURL = 'http://localhost:8000/api/v1';
+const baseURL = '/api/v1';
 
 export const axiosInstance = axios.create({
   baseURL,
